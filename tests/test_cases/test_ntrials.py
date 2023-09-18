d99c723c7c41568eaa717cdb558a18e2cffc27ed
brian <b9andrews@gmail.com>
2023-09-18 16:53:59 -0400

    change input for ```test_lenNTrials``` and added docstring

--- tests/test_cases/test_ntrials.py
+++ tests/test_cases/test_ntrials.py
@@ -3,16 +3,21 @@ sys.path.append('../../')
 import os
 import unittest
 from src.qodon.initiate_sequences import GenerateInitialSequences
+from Bio import SeqIO
 
 class TestNTrials(unittest.TestCase):
+    """
+    Test variable ntrials will generate appropriate number of sequences
+
+    """
     def test_lenNTrials_5(self):
-        self.seq = "GGG"
+        self.seq = str(SeqIO.read('tests/test_sequences/GGG.fasta','fasta').seq)
         self.ntrials = 5
         self.codons = GenerateInitialSequences(self.seq, self.ntrials)
         self.assertEqual(len(self.codons.initial_sequences), 5)
 
     def test_lenNTrials_10(self):
-        self.seq = "GGG"
+        self.seq = str(SeqIO.read('tests/test_sequences/GGG.fasta','fasta').seq)
         self.ntrials = 10
         self.codons = GenerateInitialSequences(self.seq, self.ntrials)
         self.assertEqual(len(self.codons.initial_sequences), 10)
