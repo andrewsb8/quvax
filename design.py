8cd254084a393f55551365714450a05f12cf94d7
bandrews <b9andrews@gmail.com>
2023-11-14 14:36:00 -0500

    finally address #5 and it works

--- design.py
+++ design.py
@@ -1,18 +1,17 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' #removes tensorflow warnings
 from src.params.parser import Parser
-from src.qodon.optimizers.tf_differential_evo import TfDiffEv
-from src.qodon.optimizers.classical_ga import GeneticAlgorithm
-from src.qodon.optimizers.random_optimizer import RandomOptimizer
 
 if __name__ == "__main__":
     config = Parser()
 
     if config.args.codon_optimizer == "TFDE":
+        from src.qodon.optimizers.tf_differential_evo import TfDiffEv
         TfDiffEv(config)
     elif config.args.codon_optimizer == "GA":
+        from src.qodon.optimizers.classical_ga import GeneticAlgorithm
         GeneticAlgorithm(config)
     elif config.args.codon_optimizer == "RAND":
+        from src.qodon.optimizers.random_optimizer import RandomOptimizer
         RandomOptimizer(config)
     else:
         print("Error here.")
