ef8bc2c21ceacfeef28b2bb0692298854cc8fe63
bandrews <b9andrews@gmail.com>
2023-11-27 13:11:15 -0500

    added tests with longer sequences for more comprehensive testing

--- tests/test_cases/test_random_seed.py
+++ tests/test_cases/test_random_seed.py
@@ -28,7 +28,7 @@ class TestRandomSeed(unittest.TestCase):
 
     def test_TfDiffEv_DefaultSeed(self):
         from src.qodon.optimizers.tf_differential_evo import TfDiffEv
-        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-n", "4", "-c", "10000", "-sd", "1"]
+        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-n", "4", "-c", "1000", "-sd", "1"]
         with patch.object(sys, 'argv', testargs):
             parser = Parser()
             opt = TfDiffEv(parser)
@@ -37,7 +37,7 @@ class TestRandomSeed(unittest.TestCase):
 
     def test_TfDiffEv_NewSeed(self):
         from src.qodon.optimizers.tf_differential_evo import TfDiffEv
-        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "TFDE", "-n", "4", "-c", "10000", "-sd", "2546345746583"]
+        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "TFDE", "-n", "4", "-c", "1000", "-sd", "2546345746583"]
         with patch.object(sys, 'argv', testargs):
             parser = Parser()
             opt = TfDiffEv(parser)
@@ -51,11 +51,20 @@ class TestRandomSeed(unittest.TestCase):
             parser = Parser()
             opt = TfDiffEv(parser)
             opt2 = TfDiffEv(parser)
+            self.assertEqual(opt.optimization_process['sequences'][-1], opt2.optimization_process['sequences'][-1])
+
+    def test_TfDiffEv_LongerSequence(self):
+        from src.qodon.optimizers.tf_differential_evo import TfDiffEv
+        testargs = ["design.py", "-i", "examples/spike_trim_20.fasta", "-co", "TFDE", "-n", "4", "-c", "1000", "-sd", "1"]
+        with patch.object(sys, 'argv', testargs):
+            parser = Parser()
+            opt = TfDiffEv(parser)
+            opt2 = TfDiffEv(parser)
             self.assertEqual(opt.final_codons, opt2.final_codons)
 
     def test_GA_DefaultSeed(self):
         from src.qodon.optimizers.classical_ga import GeneticAlgorithm
-        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "GA", "-n", "4", "-c", "10000", "-sd", "1"]
+        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "GA", "-n", "4", "-c", "1000", "-sd", "1"]
         with patch.object(sys, 'argv', testargs):
             parser = Parser()
             opt = GeneticAlgorithm(parser)
@@ -64,7 +73,7 @@ class TestRandomSeed(unittest.TestCase):
 
     def test_GA_NewSeed(self):
         from src.qodon.optimizers.classical_ga import GeneticAlgorithm
-        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "GA", "-n", "4", "-c", "10000", "-sd", "2546345746583"]
+        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "GA", "-n", "4", "-c", "1000", "-sd", "2546345746583"]
         with patch.object(sys, 'argv', testargs):
             parser = Parser()
             opt = GeneticAlgorithm(parser)
@@ -78,11 +87,20 @@ class TestRandomSeed(unittest.TestCase):
             parser = Parser()
             opt = GeneticAlgorithm(parser)
             opt2 = GeneticAlgorithm(parser)
+            self.assertEqual(opt.optimization_process['sequences'][-1], opt2.optimization_process['sequences'][-1])
+
+    def test_GA_LongerSequence(self):
+        from src.qodon.optimizers.classical_ga import GeneticAlgorithm
+        testargs = ["design.py", "-i", "examples/spike_trim_20.fasta", "-co", "GA", "-n", "4", "-c", "1000", "-sd", "1"]
+        with patch.object(sys, 'argv', testargs):
+            parser = Parser()
+            opt = GeneticAlgorithm(parser)
+            opt2 = GeneticAlgorithm(parser)
             self.assertEqual(opt.final_codons, opt2.final_codons)
 
     def test_RAND_DefaultSeed(self):
         from src.qodon.optimizers.random_optimizer import RandomOptimizer
-        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "RAND", "-n", "4", "-c", "10000", "-sd", "1"]
+        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "RAND", "-n", "4", "-c", "1000", "-sd", "1"]
         with patch.object(sys, 'argv', testargs):
             parser = Parser()
             opt = RandomOptimizer(parser)
@@ -91,7 +109,7 @@ class TestRandomSeed(unittest.TestCase):
 
     def test_RAND_NewSeed(self):
         from src.qodon.optimizers.random_optimizer import RandomOptimizer
-        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "RAND", "-n", "4", "-c", "10000", "-sd", "2546345746583"]
+        testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "RAND", "-n", "4", "-c", "1000", "-sd", "2546345746583"]
         with patch.object(sys, 'argv', testargs):
             parser = Parser()
             opt = RandomOptimizer(parser)
@@ -101,6 +119,15 @@ class TestRandomSeed(unittest.TestCase):
     def test_RAND_OneIteration(self):
         from src.qodon.optimizers.random_optimizer import RandomOptimizer
         testargs = ["design.py", "-i", "tests/test_sequences/GGGN.fasta", "-co", "RAND", "-n", "4", "-c", "1", "-sd", "1"]
+        with patch.object(sys, 'argv', testargs):
+            parser = Parser()
+            opt = RandomOptimizer(parser)
+            opt2 = RandomOptimizer(parser)
+            self.assertEqual(opt.optimization_process['sequences'][-1], opt2.optimization_process['sequences'][-1])
+
+    def test_RAND_LongerSequence(self):
+        from src.qodon.optimizers.random_optimizer import RandomOptimizer
+        testargs = ["design.py", "-i", "examples/spike_trim_20.fasta", "-co", "RAND", "-n", "4", "-c", "1000", "-sd", "1"]
         with patch.object(sys, 'argv', testargs):
             parser = Parser()
             opt = RandomOptimizer(parser)
