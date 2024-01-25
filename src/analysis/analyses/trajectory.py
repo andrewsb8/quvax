20efa5a9b95bc38ea8630e788e8ad8249221b7a6
bandrews <b9andrews@gmail.com>
2024-01-25 13:24:00 -0500

    added trajectory tools which need to be fixed to output multiple files on their own

---
+++ src/analysis/analyses/trajectory.py
@@ -0,0 +1,31 @@
+from src.analysis.analysis import Analysis
+import numpy as np
+
+
+class Trajectory(Analysis):
+    """
+    Plotting the free energy changes as a function of codon optimization step
+    relative to the initial sequence of the population. This analysis will
+    produce a number of output files equal to the population size(n_trials in
+    design.py).
+
+    Parameters
+    ----------
+    """
+
+    def __init__(self, config):
+        super().__init__(config)
+        self._analyze()
+
+    def _analyze(self):
+        dumb = 9
+        self.iterations = np.arange(1, len(self.config.data["sequences"])/self.config.data["generation_size"])
+
+        self.energy_diff = [
+            self._calc_energy_diff(self.config.data["energies"][dumb], self.config.data["energies"][k+self.config.data["generation_size"]]) for k in range(dumb, len(self.config.data["sequences"])-self.config.data["generation_size"], self.config.data["generation_size"])
+        ]
+
+        self._generate_output_2D([self.iterations, self.energy_diff])
+
+    def _calc_energy_diff(self, ref_energy, energy):
+        return energy - ref_energy
--- src/params/analysis_parser.py
+++ src/params/analysis_parser.py
@@ -60,7 +60,7 @@ class AnalysisParser(object):
             "--analysis_type",
             default="fe_landscape",
             type=str,
-            help="Specify type of analysis. Values: fe_landscape, fe_generation",
+            help="Specify type of analysis. Values: fe_landscape, fe_generation, trajectory, trajectory_per_generation",
         )
         self.parser.add_argument(
             "-l",
