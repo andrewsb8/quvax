c7c18dc90c0c642ceca4132b45f9b82a2ec58c03
bandrews <b9andrews@gmail.com>
2024-04-08 16:19:33 -0400

    removed type hint from random optimizer...

--- src/qodon/optimizers/random_optimizer.py
+++ src/qodon/optimizers/random_optimizer.py
@@ -10,7 +10,7 @@ class RandomOptimizer(CodonOptimizer):
 
     """
 
-    def __init__(self, config : DesignParser):
+    def __init__(self, config):
         super().__init__(config)
         self._optimize()
 
