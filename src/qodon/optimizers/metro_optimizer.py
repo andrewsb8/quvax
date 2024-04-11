3fefb792858c5741aea6dddaddddbdfac270bd32
Jake Abraham <jakeabraham@jakes-air-2.haverford.edu>
2024-04-11 12:36:05 -0400

    add metroplolis file

---
+++ src/qodon/optimizers/metro_optimizer.py
@@ -0,0 +1,27 @@
+from src.qodon.optimizer import CodonOptimizer
+
+class MetropolisOptimizer(CodonOptimizer):
+
+    """
+    Implementation of the Monte Carlo Metropolis Algorithm for a codon sequence
+
+    Parameters
+
+    -----------
+
+    """
+    
+    def __init__(self, config):
+        super().__init__(config)
+        self.optimize()
+
+    def _optimize(self):
+        """
+
+        Method for codon optimization using metropolis algorithm
+
+        """
+
+        
+        
+    
