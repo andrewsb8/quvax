53ce44f8ec0f2011b44d7ca486bb4134eaf64e55
Jake Abraham <jakeabraham@jakes-air-2.haverford.edu>
2024-04-12 10:51:37 -0400

    add metroplolis file

--- src/qodon/optimizers/metro_optimizer.py
+++ src/qodon/optimizers/metro_optimizer.py
@@ -19,9 +19,18 @@ class MetropolisOptimizer(CodonOptimizer):
         """
 
         Method for codon optimization using metropolis algorithm
+        
+        """
+        
+        n_seqs = [self.reverse_translate(s) for s in self.initial_sequences] #storing the nucleotide sequences
+        energies = [self._fold_rna(s) for s in n_seqs] #getting the folding energies of these sequences
+        self._extend_output(n_seqs, energies, None)
+        
+        """
+        
+        All of this is taken from the random optimizer script so far.
 
         """
-
-        
-        
     
+        for i in range(self.config.args.codon_iterations):
+            
