e2a8e10dc0e3d89783a6b2c9349a5a3466dea261
bandrews <b9andrews@gmail.com>
2024-05-07 15:12:29 -0400

    move folder constructor to base class

--- src/rna_folding/rna_folder.py
+++ src/rna_folding/rna_folder.py
@@ -12,5 +12,16 @@ class RNAFolder(ABC):
 
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, config):
+        self.config = config
+        self.interactions = [
+            ("A", "U"),
+            ("U", "A"),
+            ("G", "C"),
+            ("C", "G"),
+            ("G", "U"),
+            ("U", "G"),
+        ]
+        self.twobody_penalty = 500000
+        self.pseudo_factor = 0.5
+        self.no_stem_penalty = 500000
--- src/rna_folding/rna_folders/simulated_annealer.py
+++ src/rna_folding/rna_folders/simulated_annealer.py
@@ -39,18 +39,7 @@ class SimulatedAnnealer(RNAFolder):
     """
 
     def __init__(self, config: DesignParser):
-        self.config = config
-        self.interactions = [
-            ("A", "U"),
-            ("U", "A"),
-            ("G", "C"),
-            ("C", "G"),
-            ("G", "U"),
-            ("U", "G"),
-        ]
-        self.twobody_penalty = 500000
-        self.pseudo_factor = 0.5
-        self.no_stem_penalty = 500000
+        super().__init__(config)
 
     def _fold(self, sequence):
         self.nseq = sequence
