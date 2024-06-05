32a0572dc11d72ba79518d42f70c9025ca4724aa
bandrews <b9andrews@gmail.com>
2024-06-05 13:26:42 -0400

    all stems are being identified as pseudoknots now

--- src/rna_folding/rna_folder.py
+++ src/rna_folding/rna_folder.py
@@ -187,7 +187,7 @@ class RNAFolder(ABC):
     def _stems_to_dot_bracket(self, sequence_len, stems):
         """
         Function to convert a list of stems in a sequence to a dot-bracket
-        notation. No pseudoknots implemented yet.
+        notation.
 
         """
 
@@ -197,5 +197,17 @@ class RNAFolder(ABC):
             for i in range(len(stem_pair_list)):
                 dot_bracket[stem_pair_list[i][0] - 1] = "("
                 dot_bracket[stem_pair_list[i][1] - 1] = ")"
+
+        #check for pseudoknots
+        #turns everything into pseudoknots!
+        for i in range(len(stems)):
+            for j in range(i, len(stems)):
+                if self._is_pseudo(stems[i], stems[j]):
+                    pseudo_stems = [stems[i], stems[j]]
+                    for stem in pseudo_stems:
+                        stem_pair_list = self._stem_to_pair_list(stem)
+                        for k in range(len(stem_pair_list)):
+                            dot_bracket[stem_pair_list[k][0] - 1] = "["
+                            dot_bracket[stem_pair_list[k][1] - 1] = "]"
 
         self.dot_bracket = "".join(dot_bracket)
