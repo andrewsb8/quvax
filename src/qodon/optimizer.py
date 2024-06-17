997a495a26ff60209b7dd8a5489f6d94c033b040
Brian <b9andrews@gmail.com>
2024-06-17 13:21:55 -0400

    remove print

--- src/qodon/optimizer.py
+++ src/qodon/optimizer.py
@@ -250,11 +250,7 @@ class CodonOptimizer(ABC):
             self.sec_structs.append(self.folder.dot_bracket)
         self._update_mfe(self.energies)
         self._write_output(self.list_seqs, self.energies, self.sec_structs)
-        if (
-            self.codon_optimize_step != 0
-            and self.codon_optimize_step % self.config.args.checkpoint_interval == 0
-            and self.codon_optimize_step != self.config.args.codon_iterations
-        ):
+        if self.codon_optimize_step != 0 and self.codon_optimize_step % self.config.args.checkpoint_interval == 0 and self.codon_optimize_step != self.config.args.codon_iterations:
             self.config.log.info("Writing checkpoint:")
             self._post_process()
             self.config.log.info("")
@@ -302,7 +298,6 @@ class CodonOptimizer(ABC):
             step = self.codon_optimize_step + self.config.generations_sampled
         else:
             step = self.codon_optimize_step
-        print(step)
         self.config.db_cursor.execute("SELECT COUNT(DISTINCT sequences) from OUTPUTS;")
         num = self.config.db_cursor.fetchall()[0][0]
         # "+ self.config.args.n_trials" to account for initial randomly generated sequences
