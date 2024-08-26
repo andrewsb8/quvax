ae3c29fad1242f681993e3d4ea135e47d86895e7
bandrews <b9andrews@gmail.com>
2024-08-26 12:51:31 -0400

    add test for random seed of MC folder

--- tests/test_cases/test_random_seed.py
+++ tests/test_cases/test_random_seed.py
@@ -417,3 +417,22 @@ def test_METRO_DefaultSeed():
     opt2 = MetropolisOptimizer(parser)
     opt2._optimize()
     assert opt.list_seqs == opt2.list_seqs
+
+def test_Fold_MC():
+    """
+    Test to verify the same minimum energy and secondary structure are reached
+    on repeated runs of fold.py
+    """
+    from src.params.fold_parser import FoldParser
+    from src.rna_folding.rna_folders.classical_mc import MC
+
+    testargs = [
+        "-i",
+        "AUGACUAGGUAUCUAUCUUAU",
+    ]
+    parser = FoldParser(testargs)
+    folder = MC(parser)
+    folder._fold(folder.config.args.input)
+    folder2 = MC(parser)
+    folder2._fold(folder2.config.args.input)
+    assert folder.best_score == folder2.best_score and folder.dot_bracket == folder2.dot_bracket
