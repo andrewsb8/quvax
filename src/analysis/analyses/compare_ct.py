096b03525930b896fe5c57d225b7429ea2dfa837
Jake Abraham <jakeabraham@jakes-air-2.haverford.edu>
2024-09-18 13:59:09 -0400

    renaming and minor fixes

--- src/analysis/analyses/compare_ct.py
+++ src/analysis/analyses/compare_ct.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-
 def ct_to_dataframe(ct_file):
     """
-    This function takes base connectivity data in the form of .ct files and converts it to
-    a data frame.
+    Takes base connectivity data in the form of .ct files and converts it to a data frame.
     
     """
     df = pd.read_csv(ct_file, delim_whitespace=True, skiprows=1, header=None)
@@ -12,8 +10,8 @@ def ct_to_dataframe(ct_file):
     return df
 
 #accessing the files from my desktop, will have to be changed when it enters the codebase
-#ct_file_path_target = '/Users/jakeabraham/Desktop/5s_Acetobacter-sp.-1_archiveII.ct'
-#ct_file_path_quvax = '/Users/jakeabraham/Desktop/5s_Acetobacter-sp.-1_quvax.ct'
+ct_file_path_target = '/Users/jakeabraham/Desktop/5s_Acetobacter-sp.-1_archiveII.ct'
+ct_file_path_quvax = '/Users/jakeabraham/Desktop/5s_Acetobacter-sp.-1_quvax.ct'
 
 targetct = ct_to_dataframe(ct_file_path_target)
 targetpairings = targetct["Paired With"]
@@ -21,34 +19,32 @@ targetpairings = targetct["Paired With"]
 quvaxct = ct_to_dataframe(ct_file_path_quvax)
 quvaxpairings = quvaxct["Paired With"]
 
-
 def compare_tables(test_pairings, ref_pairings):
     """
-    Function that inputs the base pairing data from both our code and from a reference database, 
-    and outputs the sensitivity, PPV, F1 score, specificity, bases correctly paired, and .
+    Inputs the data frame, outputs the sensitivity, PPV, F1 score, 
+    specificity, bases correctly paired, and bases with incorrect pairing.
 
     """
-#    truepos, trueneg, falsepos, falseneg = truthvalues(test_pairings,ref_pairings)
     print("Bases correctly paired: ", truepos)
     print("Bases with misidentified pairings (either missed or incorrect pairing): ", falsepos + falseneg)
-    print("Sensitivity: ", sensitivity(test_pairings, ref_pairings), " ; PPV: ", PPV(test_pairings, ref_pairings)," ; F1: ", F1(test_pairings, ref_pairings), " ; Specificity: ", specificity(test_pairings, ref_pairings))
+    print("Sensitivity: ", sensitivity(truepos, falseneg), " ; PPV: ", PPV(truepos, falsepos)," ; F1: ", F1(truepos, falsepos, falseneg), " ; Specificity: ", specificity(trueneg, falsepos))
 
-def sensitivity(test_pairings, ref_pairings):
+def sensitivity(truepos, falseneg):
     return truepos/(truepos+falseneg)
 
-def PPV(test_pairings, ref_pairings):
+def PPV(truepos, falsepos):
     return truepos/(truepos+falsepos)
     
-def F1(test_pairings, ref_pairings):
+
+def F1(truepos, falsepos, falseneg):
     return 2*truepos/(2*truepos + falsepos + falseneg)
 
-def specificity(test_pairings, ref_pairings):
+def specificity(trueneg, falsepos):
     return trueneg/(trueneg+falsepos)
     
-  
 def truthvalues(test_pairings, ref_pairings):
     """
-    This function gathers data from the tables to find how many true positives, true negatives, 
+    Gathers data from the tables to find how many true positives, true negatives, 
     false positives, and false negatives there are given both data sets . 
     
     """
@@ -59,16 +55,13 @@ def truthvalues(test_pairings, ref_pairings):
     for i in range(len(test_pairings)):
         if test_pairings[i] != 0 and test_pairings[i] == ref_pairings[i]:
             numtruepos += 1
-    for i in range(len(test_pairings)):
         if test_pairings[i] == 0 and ref_pairings[i] == 0:
             numtrueneg += 1
-    for i in range(len(test_pairings)):
         if test_pairings[i] != 0 and test_pairings[i] != ref_pairings[i]:
-            numfalsepos += 1
-    for i in range(len(test_pairings)):
+                numfalsepos += 1
         if test_pairings[i] == 0 and ref_pairings[i] != 0:
             numfalseneg += 1
     return numtruepos, numtrueneg, numfalsepos, numfalseneg
 
 truepos, trueneg, falsepos, falseneg = truthvalues(quvaxpairings,targetpairings)
-compare_tables(quvaxpairings,targetpairings) #first entry should be our data, and the second should be the database
+compare_tables(quvaxpairings,targetpairings) 
