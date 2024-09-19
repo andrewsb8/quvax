a97afd602f2de1b987249ac622ee1ceb20b20b08
bandrews <b9andrews@gmail.com>
2024-09-19 10:45:58 -0400

    modify class name and docstrings

--- src/analysis/analyses/compare_ct.py
+++ src/analysis/analyses/compare_ct.py
@@ -1,10 +1,9 @@
-#Class version 
 from src.analysis.analysis import Analysis
 import pandas as pd
 
-class Compare_CT(Analysis):
+class CompareCT(Analysis):
     """
-    Analyzes CT table output from Quvax and compares with database. Outputs sensitivity, PPV, 
+    Compares two connectivity tables. Outputs sensitivity, PPV,
     F1 score, specificity, bases correctly paired, and bases with incorrect pairing.
 
     Parameters
@@ -13,24 +12,24 @@ class Compare_CT(Analysis):
         Object containing user inputs
 
     """
-    
+
     def __init__(self, config):
         super().__init__(config)
-        self.truepos = 0 
-        self.trueneg = 0 
-        self.falsepos = 0 
-        self.falseneg = 0 
+        self.truepos = 0
+        self.trueneg = 0
+        self.falsepos = 0
+        self.falseneg = 0
         self._analyze()
 
     def _analyze(self):
         input_pairings, reference_pairings = self.get_pairings(self.config.args.input, self.config.args.reference)
         truepos, trueneg, falsepos, falseneg = self.truthvalues(input_pairings,reference_pairings)
-        self.compare_tables(self.truepos, self.trueneg, self.falsepos, self.falseneg) 
-        
+        self.compare_tables(self.truepos, self.trueneg, self.falsepos, self.falseneg)
+
     def ct_to_dataframe(self, ct_file):
         """
-        Takes base connectivity data in the form of .ct files and converts it to a data frame.
-        
+        Convert connectivity table to dataframe
+
         """
         df = pd.read_csv(ct_file, delim_whitespace=True, skiprows=1, header=None)
         df.columns = ["Index", "Nucleotide", "Previous", "Next", "Paired With", "Counter"]
@@ -38,10 +37,10 @@ class Compare_CT(Analysis):
 
     def get_pairings(self, input_ct_file, reference_ct_file)
         """
-        Takes user-input .ct files and obtains base pairing data from it.
-        
+        Returns base pair information from connectivity tables
+
         """
-        reference_ct = self.ct_to_dataframe(reference_ct_file) 
+        reference_ct = self.ct_to_dataframe(reference_ct_file)
         ref_pairings = reference_ct["Paired With"]
         input_ct = self.ct_to_dataframe(input_ct_file)
         in_pairings = input_ct["Paired With"]
@@ -49,31 +48,31 @@ class Compare_CT(Analysis):
 
     def compare_tables(self, truepos, trueneg, falsepos, falseneg):
         """
-        Inputs the data frame, outputs the sensitivity, PPV, F1 score, 
+        Calculates sensitivity, PPV, F1 score,
         specificity, bases correctly paired, and bases with incorrect pairing.
-    
+
         """
         print("Bases correctly paired: ", str(truepos))
         print("Bases with misidentified pairings (either missed or incorrect pairing): ", str(falsepos + falseneg))
         print("Sensitivity: ", str(self.sensitivity(truepos, falseneg)), " ; PPV: ", str(self.PPV(truepos, falsepos))," ; F1: ", str(self.F1(truepos, falsepos, falseneg)), " ; Specificity: ", str(self.specificity(trueneg, falsepos)))
-        
+
     def sensitivity(self, truepos, falseneg):
         return truepos/(truepos+falseneg)
-    
+
     def PPV(self, truepos, falsepos):
         return truepos/(truepos+falsepos)
-    
+
     def F1(self, truepos, falsepos, falseneg):
         return 2*truepos/(2*truepos + falsepos + falseneg)
-    
+
     def specificity(self, trueneg, falsepos):
         return trueneg/(trueneg+falsepos)
-    
+
     def truthvalues(self, test_pairings, ref_pairings):
         """
-        Gathers data from the tables to find how many true positives, true negatives, 
-        false positives, and false negatives there are given both data sets . 
-        
+        Calculates true positives, true negatives, false positives, and false negatives
+        for a test set of base pairs given a reference set
+
         """
         for i in range(len(test_pairings)):
             if test_pairings[i] != 0 and test_pairings[i] == ref_pairings[i]:
