a824a6cf5c6caab4fe1c9bdb4814bd96f9ea3618
Jake Abraham <jakeabraham@macbook-pro-5.haverford.edu>
2024-11-20 12:08:14 -0500

    Changing help description to match ClassifyStems

--- src/config/analysis_config.py
+++ src/config/analysis_config.py
@@ -176,7 +176,7 @@ class AnalysisConfig(Config):
         parser_classify_stems = subparsers.add_parser(
             "classify_stems",
             parents=[ss_parser],
-            help="Calculates the average, minimum, and maximum stem length, sequence length, number of stems, stems in pseudoknots, and number of overlapping stems for an input connectivity table.",
+            help="Calculates the average, minimum, and maximum stem length, sequence length, number of stems, and number ofstems in pseudoknots for an input connectivity table.",
         )
         parser_sst = subparsers.add_parser(
             "sec_struct_types",
