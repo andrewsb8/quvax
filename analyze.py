563013c3a4bbd9e26dac695fc57b822ef0904d6a
bandrews <b9andrews@gmail.com>
2024-12-05 17:35:04 -0500

    more merge conflicts

--- analyze.py
+++ analyze.py
@@ -27,8 +27,7 @@ if __name__ == "__main__":
     elif config.args.command == "sec_struct_types":
         from src.analysis.analyses.base_pair_types import BasePairTypes
 
-<<<<<<< HEAD
-        SecondaryStructureTypes(config)
+        BasePairTypes(config)
     elif config.args.command == "compute_energy":
         from src.analysis.analyses.compute_energy import ComputeEnergy
 
@@ -37,9 +36,6 @@ if __name__ == "__main__":
         from src.analysis.analyses.k_neighbor_energy import kNeighborEnergySearch
 
         kNeighborEnergySearch(config)
-=======
-        BasePairTypes(config)
->>>>>>> main
     elif config.args.command == "base_pair_ranges":
         from src.analysis.analyses.base_pair_ranges import BasePairRanges
 
